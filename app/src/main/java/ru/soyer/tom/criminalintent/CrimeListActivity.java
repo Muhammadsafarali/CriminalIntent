1e548faa0629746e8de0490a678048890652bd67
Safarali <dr.safarali@yandex.ru>
2018-08-09 06:56:55 +0300

    Глава 17. Реализация CrimeListFragment.Callbacks

--- app/src/main/java/ru/soyer/tom/criminalintent/CrimeListActivity.java
+++ app/src/main/java/ru/soyer/tom/criminalintent/CrimeListActivity.java
@@ -1,16 +1,35 @@
 package ru.soyer.tom.criminalintent;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 
 /**
  * Created by elenaozerova on 25/05/2018.
  */
 
-public class CrimeListActivity extends SingleFragmentActivity {
+public class CrimeListActivity extends SingleFragmentActivity implements CrimeListFragment.Callbacks {
 
     @Override
     protected Fragment createFragment() {
         return new CrimeListFragment();
     }
 
+    @Override
+    protected int getLayoutResId() {
+        return R.layout.activity_masterdetail;
+    }
+
+    @Override
+    public void onCrimeSelected(Crime crime) {
+        if (findViewById(R.id.detail_fragment_container) == null) {
+            Intent intent = CrimePagerActivity.newIntent(this, crime.getId());
+            startActivity(intent);
+        } else {
+            Fragment newDetail = CrimeFragment.newInstance(crime.getId());
+
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.detail_fragment_container, newDetail)
+                    .commit();
+        }
+    }
 }
